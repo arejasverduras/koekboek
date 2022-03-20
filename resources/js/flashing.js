3893978e0a60c6b87fd59289aba66fdd474c08da
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-03-20 13:40:36 +0100

    write changecolor loop

--- resources/js/flashing.js
+++ resources/js/flashing.js
@@ -1,14 +1,35 @@
 //one
 //create a random color
+const generateRGBColor = () =>{
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+
+    const rgb = r+","+g+","+b;
+    console.log(rgb);
+    return rgb;
+}
+
+let color = generateRGBColor();
 
 //two
 //set the element to change to a varibale
+let element = document.getElementsByTagName('h1')[0];
 
 //three
 //write a function that changes the variable style to the random color
+const changeColor = (element,color)=>{
+    element.style.color = "rgb("+color+")";
+}
+
+changeColor(element,color);
 
 //four
 //write a loop that keeps repeating this function
+for (let x = 0;x>0;x++) {
+    setTimeout(changeColor,500);
+}
+
 
 //five
 // create a delay between loop iterations
