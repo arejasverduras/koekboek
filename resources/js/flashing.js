985d8a6058ca50513a6dd347680c168cf286c62e
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-03-20 12:38:24 +0100

    write script structure

---
+++ resources/js/flashing.js
@@ -0,0 +1,26 @@
+//one
+//create a random color
+
+//two
+//set the element to change to a varibale
+
+//three
+//write a function that changes the variable style to the random color
+
+//four
+//write a loop that keeps repeating this function
+
+//five
+// create a delay between loop iterations
+
+//six
+//export this function
+
+//seven
+//import this function
+
+//eight 
+//inititalize this function on page load
+
+//nine (extra)
+//initialize this functino for the footer, after the footer element is generated using the koken() function;
